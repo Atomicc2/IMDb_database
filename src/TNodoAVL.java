db4d1a79cd97fe4780a7eec2dacd824d377dc486
svgabriiel <svgabriiel@gmail.com>
2026-07-07 17:14:33 -0300

    Classes Arvore criadas Analisar por favor!!

---
+++ src/TNodoAVL.java
@@ -0,0 +1,17 @@
+public class TNodoAVL {
+
+    public TitleBasic item;
+    public TNodoAVL esq;
+    public TNodoAVL dir;
+    public TNodoAVL pai;
+    public int altura;
+
+    public TNodoAVL(TitleBasic item, TNodoAVL pai) {
+        this.item = item;
+        this.pai = pai;
+        this.esq = null;
+        this.dir = null;
+        this.altura = 0;
+    }
+
+}
---
+++ src/TNodoBIN.java
@@ -0,0 +1,13 @@
+public class TNodoBIN {
+    TNodoBIN esq;
+    TitleBasic item;
+    TNodoBIN dir;
+    TNodoBIN pai;
+
+    public TNodoBIN(TitleBasic item, TNodoBIN pai) {
+        this.item = item;
+        this.esq = null;
+        this.dir = null;
+        this.pai = pai;
+    }
+}
